71412113370538c0b2fc6708e483c8f927aaadb1
Simon Rand <simonrand@gmail.com>
2020-09-07 22:45:41 +0100

    Updates..

---
+++ .zshrc
@@ -0,0 +1,71 @@
+################################################################ zsh Completions
+
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+  autoload -Uz compinit
+  compinit
+fi
+
+########################################################################### asdf
+
+export ASDF_DIR=$(brew --prefix asdf)
+
+######################################################################### Prompt
+
+autoload -Uz vcs_info
+precmd() { vcs_info }
+
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' unstagedstr '*'
+zstyle ':vcs_info:*' stagedstr '+'
+zstyle ':vcs_info:git:*' formats '(%b)%u%c%m '
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
+
++vi-git-untracked() {
+  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+     git status --porcelain | grep -m 1 '^??' &>/dev/null
+  then
+    hook_com[misc]='?'
+  fi
+}
+
+setopt PROMPT_SUBST
+PROMPT='%F{8}%B%~%b%f %F{175}${vcs_info_msg_0_}%f$ '
+
+############################################################## Aliases/Functions
+
+# Shell
+mcd () { mkdir -p "$1" && cd "$1"; }
+alias cleardns='sudo killall -HUP mDNSResponder'
+
+# Git
+alias gcom='git commit -m'
+alias gc='git checkout'
+alias gpo='git push origin HEAD'
+alias gst='git status'
+alias gl='git log'
+alias ga='git add'
+alias remove_merged_branches='git branch --merged | egrep -v "(^\*|master|staging)" | xargs git branch -d'
+
+# Hub/Git
+alias gpr='hub pull-request -o -b'
+
+# Elixir
+alias ips='iex -S mix phoenix.server'
+
+# Ruby/Rails
+alias c='bundle exec rails c'
+alias s='bundle exec rails s'
+alias g='bundle exec guard'
+alias i='irb'
+
+# Foreman
+alias f='foreman start'
+
+########################################################################### Misc
+
+# Local specific
+if [ -f ~/.zsh_local ]; then
+  source ~/.zsh_local
+fi
--- setup.sh
+++ setup.sh
@@ -1,122 +1,73 @@
 # Originally: http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac
 
 if [ "$1" == "upgrade" ]; then
-  echo "upgrading system.."
+  echo "upgrading and resetting system.."
   command=upgrade
 else
   echo "installing system.."
   command=install
 fi
 
-# Get some inputs
+######################################################################### Inputs
+
 echo Enter your email address:
 read email
-echo Enter your system username:
-read username
+username=$(whoami)
 
-# Command line tools
-echo "installing command line tools..."
+################################################################ macOS cli tools
+
+echo "installing command line tools.."
 xcode-select --install
 
-# Check for Homebrew,
-# Install if we don't have it
+####################################################################### homebrew
+
+# check for homebrew, install if we don't have it
 if test ! $(which brew); then
-  echo "Installing homebrew..."
+  echo "installing homebrew.."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Update homebrew recipes
-echo "updating homebrew..."
+# update homebrew
+echo "updating homebrew.."
 brew update
 
-# Install GNU core utilities (those that come with OS X are outdated)
-brew $command coreutils
-
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew $command findutils
-
-# Install Bash 4
-brew $command bash
-
-# Install more recent versions of some OS X tools
-brew tap homebrew/dupes
-brew $command homebrew/dupes/grep
-
 binaries=(
-  bash-completion
-  elixir
+  asdf
   git
   hub
-  mysql
-  nginx
-  node
-  postgres
   rbenv
   rbenv-gemset
   ruby-build
   wget
+  zsh-completions
 )
 
-echo "installing binaries..."
+echo "installing homebrew binaries.."
 brew $command ${binaries[@]}
 
-# Install apps
-# Apps
+# homebrew cask apps
 apps=(
-  alfred
-  banktivity
-  betterzipql
-  dash
-  dropbox
-  duet
-  google-chrome
-  google-drive
-  handbrake
   imageoptim
-  iterm2
-  polymail
-  postico
-  qbittorrent
+  nvalt
   qlcolorcode
   qlimagesize
   qlmarkdown
-  qlprettypatch
   quicklook-csv
   quicklook-json
-  safari-technology-preview
-  sequel-pro
-  sketchup
-  skype
-  slack
-  sublime-text
-  superduper
   suspicious-package
-  transmit
-  vlc
 )
 
-# Install apps to /Applications
-echo "installing apps..."
+echo "installing cask apps.."
 brew cask install ${apps[@]}
 
-# Link subl to st
-echo "linking st -> subl..."
-ln -s /usr/local/bin/subl /usr/local/bin/st
+####################################################################### dotfiles
 
-# Copy latest dotfiles
-echo "copying dotfiles"
-cp ./.bash_profile ~/.bash_profile
+echo "copying dotfiles and global configs.."
+cp ./.zshrc ~/.zshrc
 cp ./.gemrc ~/.gemrc
 cp ./.railsrc ~/.railsrc
 
-# NOTE: item2 plist will only work with /Users/simon home folder
-cp ./iterm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
-
-sed "s/\[email\]/$email/" .gitconfig > ./.gitconfig_complete
-sed -i '' "s/\[username\]/$username/" ./.gitconfig_complete
-cp ./.gitconfig_complete ~/.gitconfig && rm ./.gitconfig_complete
-cp ./.gitignore ~/.gitignore
-
+# default configs
 configs=( bundle ssh )
 for config in "${configs[@]}"
 do
@@ -124,62 +75,63 @@ do
   cp ./.$config/config ~/.$config/config
 done
 
-# Copy files
-cp ./files/iosevka-light.ttf ~/Library/Fonts/
-
-# Install Package Control for Sublime 3 and setup
-package_path=~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
-if [ ! -f "$package_path"/Package\ Control.sublime-package ]; then
-  echo "downloading sublime text package control.."
-  mkdir -p "$package_path"
-  wget https://packagecontrol.io/Package%20Control.sublime-package -O "$package_path"/Package\ Control.sublime-package
-fi
-cp ./sublime-text3/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-
-# OSX tweaks
-chflags nohidden ~/Library/
+############################################################################ SSH
 
 # Generate an SSH key (if none exist)
 if [ ! -f ~/.ssh/id_rsa ]; then
+  echo "generating ssh key.."
   mkdir ~/.ssh
   ssh-keygen -t rsa -b 4096 -C "$email"
   ssh-add ~/.ssh/id_rsa
 fi
 
-# Tidy up
+############################################################################ Git
+
+echo "setting up git.."
+sed "s/\[email\]/$email/" .gitconfig > ./.gitconfig_complete
+sed -i '' "s/\[username\]/$username/" ./.gitconfig_complete
+cp ./.gitconfig_complete ~/.gitconfig && rm ./.gitconfig_complete
+cp ./.gitignore ~/.gitignore
+
+########################################################################## Files
+
+echo "copying files.."
+cp ./files/FiraMono-Regular.otf ~/Library/Fonts/
+cp ./files/FiraMono-Bold.otf ~/Library/Fonts/
+
+################################################################### Sublime Text
+
+echo "setting up sublime text.."
+# Link subl to st
+if [[ -f /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl && ! -f /usr/local/bin/st ]]; then
+  echo "linking st -> subl..."
+  ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/st
+fi
+cp ./sublime-text3/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+
+################################################################### macOS tweaks
+
+echo "tweaking macOS.."
+
+# Show ~/Library folder
+chflags nohidden ~/Library
+[[ $(xattr ~/Library) = com.apple.FinderInfo ]] && xattr -d com.apple.FinderInfo ~/Library
+
+# Enable mouse right click
+# NOTE: Restart required
+defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
+
+# Safari dev menu
+# NOTE: Ensure terminal app has been granted full disk access
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true && defaults write com.apple.Safari IncludeDevelopMenu -bool true && defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true && defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true && defaults write -g WebKitDeveloperExtras -bool true
+
+########################################################################### tidy
+
 echo "tidying..."
 brew cleanup
 
-# App store apps
-echo "app store:"
-echo "  [ ] 1Password"
-echo "  [ ] Affinity Designer"
-echo "  [ ] Affinity Photo"
-echo "  [ ] Base"
-echo "  [ ] Clear"
-echo "  [ ] Divvy"
-echo "  [ ] iA Writer"
-echo "  [ ] Numbers"
-echo "  [ ] Pages"
-echo "  [ ] Table Tool"
-echo "  [ ] Tweetbot"
-echo "  [ ] Xcode"
-echo "..."
-
-# Safari dev menu
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true && defaults write com.apple.Safari IncludeDevelopMenu -bool true && defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true && defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true && defaults write -g WebKitDeveloperExtras -bool true
-
-# Divvy shortcuts
-echo "divvy shortcuts:"
-echo "divvy://import/YnBsaXN0MDDUAQIDBAUIVldUJHRvcFgkb2JqZWN0c1gkdmVyc2lvblkkYXJjaGl2ZXLRBgdUcm9vdIABqgkKFCssMztCSlJVJG51bGzSCwwNDlYkY2xhc3NaTlMub2JqZWN0c4AJpQ8QERITgAKABYAGgAeACN0VFhcYGRobHB0eHyALISIjJCIiJSYnJiklKl8QEnNlbGVjdGlvbkVuZENvbHVtbl8QEXNlbGVjdGlvblN0YXJ0Um93XGtleUNvbWJvQ29kZVdlbmFibGVkXWtleUNvbWJvRmxhZ3NfEBRzZWxlY3Rpb25TdGFydENvbHVtbltzaXplQ29sdW1uc1pzdWJkaXZpZGVkV25hbWVLZXlWZ2xvYmFsXxAPc2VsZWN0aW9uRW5kUm93WHNpemVSb3dzEAMQABASCRAICIADCBAHgARQ0i0uLzJYJGNsYXNzZXNaJGNsYXNzbmFtZaIwMVhTaG9ydGN1dFhOU09iamVjdFhTaG9ydGN1dN0VFhcYGRobHB0eHyALKSI0JCI2JSYnJiklKhATCRAECIADCIAE3RUWFxgZGhscHR4fIAspIjwkIiIlJicmKSUqEBQJCIADCIAE3RUWFxgZGhscHR4fIAspIkMkIkUlJicmKSUqEBcJEAYIgAMIgATdFRYXGBkaGxwdHh8gC0siTCQiIiUmJyYpJSoQBRAVCQiAAwiABNItLlNUo1RVMV5OU011dGFibGVBcnJheVdOU0FycmF5EgABhqBfEA9OU0tleWVkQXJjaGl2ZXIACAARABYAHwAoADIANQA6ADwARwBNAFIAWQBkAGYAbABuAHAAcgB0AHYAkQCmALoAxwDPAN0A9AEAAQsBEwEaASwBNQE3ATkBOwE8AT4BPwFBAUIBRAFGAUcBTAFVAWABYwFsAXUBfgGZAZsBnAGeAZ8BoQGiAaQBvwHBAcIBwwHFAcYByAHjAeUB5gHoAekB6wHsAe4CCQILAg0CDgIPAhECEgIUAhkCHQIsAjQCOQAAAAAAAAIBAAAAAAAAAFgAAAAAAAAAAAAAAAAAAAJL"
-
-echo "final notes:"
-echo "[ ] Finder - Make menu bar dark"
-echo "[ ] Finder - Add home folder to sidebar and make default folder"
-echo "[ ] Safari - Install Ghostery, Shut Up and 1Password extension"
-echo "Set up sync folders:"
-echo "[ ] Alfred"
-echo "[ ] Dash"
-echo "..."
+########################################################################### done
 
 echo "done!"
+echo "....."
+echo "full app list: ~/Library/Mobile\ Documents/com~apple~CloudDocs/Library/apps.md"
