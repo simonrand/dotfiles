56960f7c333c952a74feb11bc9a93f9313d53fa3
Simon Rand <simonrand@gmail.com>
2020-09-08 10:13:20 +0100

    Extract dotfiles script

---
+++ scripts/update_dotfiles.sh
@@ -0,0 +1,14 @@
+####################################################################### dotfiles
+
+echo "copying dotfiles and global configs.."
+cp ../.zshrc ~/.zshrc
+cp ../.gemrc ~/.gemrc
+cp ../.railsrc ~/.railsrc
+
+# default configs
+configs=( bundle ssh )
+for config in "${configs[@]}"
+do
+  mkdir -p ~/.$config
+  cp ../.$config/config ~/.$config/config
+done
--- setup.sh
+++ setup.sh
@@ -58,18 +58,7 @@ brew cask $command ${apps[@]}
 
 ####################################################################### dotfiles
 
-echo "copying dotfiles and global configs.."
-cp ./.zshrc ~/.zshrc
-cp ./.gemrc ~/.gemrc
-cp ./.railsrc ~/.railsrc
-
-# default configs
-configs=( bundle ssh )
-for config in "${configs[@]}"
-do
-  mkdir -p ~/.$config
-  cp ./.$config/config ~/.$config/config
-done
+sh ./scripts/update_dotfiles.sh
 
 ############################################################################ SSH
 
