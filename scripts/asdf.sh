2e643c8d326e529f2c682ddda8d65902cb45eac5
Simon Rand <simonrand@gmail.com>
2020-09-09 11:17:42 +0100

    Import asdf node keyring

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -4,3 +4,4 @@ echo "installing asdf plugins.."
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+sh ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
